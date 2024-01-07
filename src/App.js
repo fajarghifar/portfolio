55093386edfabba60cfb2029e7e2ec614c33a098
Fajar Ghifari <fajarghifari3@gmail.com>
2024-01-07 10:25:40 +0700

    FEAT : Added Contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Testimonials from './pages/Testimonials';
+import Contact from './pages/Contact';
 
 import AppProvider from './context/AppProvider';
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/about" element={ <About /> } />
         <Route path="/projects" element={ <Projects /> } />
         <Route path="/testimonials" element={ <Testimonials /> } />
+        <Route path="/contact" element={ <Contact /> } />
       </Routes>
     </AppProvider>
   );
---
+++ src/data/contacts.js
@@ -0,0 +1,7 @@
+const contacts = [
+  {social: 'email', href: 'mailto:fajarghifari@gmail.com', text:'fajarghifari3@gmail.com'},
+  {social: 'linkedin', href: 'https://www.linkedin.com/in/fajarghifar/', text: "in/fajarghifar"},
+  {social: 'github', href: 'https://github.com/fajarghfiar', text: "/fajarghfiar"}
+];
+
+export default contacts;
